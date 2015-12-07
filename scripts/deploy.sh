df73cb9577fcab4d3e8cc2d93d1bb25a634b04f8
Martin Evgeniev <baltar88@gmail.com>
2015-12-08 00:49:09 +0100

    fix (app): Bad name for example app feat (clarity): Improve scripts format..

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-MARATHON=$(docker exec -it dockermesos_marathon_1 ip addr show |grep -w inet |grep -v 127.0.0.1|awk '{ print $2}'| cut -d "/" -f 1)
-curl -s -X POST http://$MARATHON:8080/v2/apps -d @apps/$1.json -H "Content-type: application/json" | python -m json.tool
+MARATHON=$(docker inspect --format '{{ .NetworkSettings.Networks.dockermesos.IPAddress }}' dockermesos_marathon_1)
+
+  curl -s -X POST http://$MARATHON:8080/v2/apps \
+  -d @apps/$1.json \
+  -H "Content-type: application/json" | \
+  python -m json.tool
--- scripts/get_ip.sh
+++ scripts/get_ip.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 
 for i in master marathon; do
-URL=$(docker exec -it dockermesos_$i\_1 ip addr show |grep -w inet |grep -v 127.0.0.1|awk '{ print $2}'| cut -d "/" -f 1)
-echo -e "$i: http://$URL:PORT"
+  URL=$(docker inspect --format '{{ .NetworkSettings.Networks.dockermesos.IPAddress }}' dockermesos_$i\_1)
+  echo -e "$i: http://$URL:PORT"
 done
+
 echo -e "Mesos Master port is 5050 and Marathon 8080"
