888e59a77a2f9d5e8ff0dd295673d73a7918ef03
Martin Evgeniev <baltar88@gmail.com>
2016-02-24 21:35:19 +0100

    Update docker-compose to v2 & use net host instead custom network..

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-MARATHON=$(docker inspect --format '{{ .NetworkSettings.Networks.dockermesos.IPAddress }}' dockermesos_marathon_1)
-
-  curl -s -X POST http://$MARATHON:8080/v2/apps \
+  curl -s -X POST http://127.0.0.1:8080/v2/apps \
   -d @apps/$1.json \
   -H "Content-type: application/json" | \
   python -m json.tool
--- scripts/get_ip.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-for i in master marathon slave; do
-  URL=$(docker inspect --format '{{ .NetworkSettings.Networks.dockermesos.IPAddress }}' dockermesos_$i\_1)
-  echo -e "$i: http://$URL:PORT"
-done
-
-echo -e "Mesos Master port is 5050 and Marathon 8080"
